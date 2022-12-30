4b30261705800dca0d1f849ec0cf849869879def
Martin Kask <martin@industrial.ninja>
2022-12-30 08:29:15 +0200

    Do not log all subs

--- src/TradingViewAPI.ts
+++ src/TradingViewAPI.ts
@@ -41,7 +41,7 @@ export class TradingViewAPI {
   public async getTicker(simpleOrProName: string): Promise<TickerSubscription> {
     const tickers = this.subscriptionMap.get(simpleOrProName);
 
-    console.debug('All subscriptions: ', this.subscriptionMap);
+    //console.debug('All subscriptions: ', this.subscriptionMap);
     if (tickers) {
       console.debug('multiple tickers' + tickers.size + ' ' + simpleOrProName);
       console.debug('TickersList: ', tickers);
